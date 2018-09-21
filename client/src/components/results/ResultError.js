8af4af37058fe1549016e07b645c34a9870663b1
aamadeo27 <aamadeo27>
2018-09-20 21:31:54 -0400

    Starting. To do: change api calls in frontend

--- client/src/components/results/ResultError.js
+++ client/src/components/results/ResultError.js
@@ -26,6 +26,7 @@ export default class extends Component {
 		const glyph = expanded ? "collapse-up" : "collapse-down"
 
 		let title = ((queryConfig.select && queryConfig.select.original) || queryConfig.query)
+		title = title || queryConfig.describe ? queryConfig.table : queryConfig.invocation
 		title = title.substring(0, 100) +  (title.length < 200 ? '' : '...')
 		title = <div className="text-left result-bar">
 			<div className="result-title" onClick={onToggleShow}>
